581c4578f0a9e425ba16f2487ae80bc047875391
David Jones <david.jones@affiliatewindow.com>
2017-05-28 15:38:20 +0100

    Added a singleton example

--- src/main/scala/designpatterns/singleton/Singleton.scala
+++ src/main/scala/designpatterns/singleton/Singleton.scala
@@ -4,5 +4,20 @@ object Singleton {
 
     def main(args: Array[String]) {
         println("--- Singleton ---")
+
+        println(MySingleton.getMyObject)
+        println(MySingleton.getMyObject) // Make sure they are the same
     }
 }
+
+
+object MySingleton {
+
+    val myObject = new MySingleton()
+
+    def getMyObject = {
+        myObject
+    }
+}
+
+class MySingleton
